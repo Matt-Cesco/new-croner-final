ba4690a6e044f8fab199d0a0b5c4677e9fe56505
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-12-03 22:18:00 +0000

    add some code

--- pages/_app.js
+++ pages/_app.js
@@ -12,12 +12,13 @@ const poppins = Poppins({
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script id="google-tag" strategy="lazyOnload">
-        src=
-        {`https://www.googletagmanager.com/gtag/js?id=${process.envNEXT_PUBLIC_GOOGLE_ANALITYCS}`}
-      </Script>
+      <Script
+        id="google-tag"
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.envNEXT_PUBLIC_GOOGLE_ANALITYCS}`}
+      />
 
-      <Script id="google-analytics" strategy="lazyOnload">
+      <Script id="google-analytics" strategy="afterInteractive">
         {` window.dataLayer = window.dataLayer || [];
          function gtag(){dataLayer.push(arguments);}
          gtag('js', new Date());
