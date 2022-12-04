e3b533ff9faffe659c5785341beb8c53228068e4
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-12-04 16:50:58 +0000

    add some code hotjjj

--- pages/_app.js
+++ pages/_app.js
@@ -26,15 +26,15 @@ export default function MyApp({ Component, pageProps }) {
          gtag('config', 'G-45E6DTPQCV');
       `}
       </Script>
-      <Script id="hotjar" strategy="lazyOnload">
-        {`(function(h,o,t,j,a,r){
+      <Script id="hotjar-script" strategy="lazyOnload">
+        {` (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:3275002,hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
             a.appendChild(r);
-         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+         })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv='); `}
       </Script>
       <main className={poppins.className}>
         <Component {...pageProps} />
