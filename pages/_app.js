16569b6d700ded1f13c11624d5a651acc8c959ed
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-12-04 16:43:05 +0000

    hot jar id

--- pages/_app.js
+++ pages/_app.js
@@ -26,8 +26,8 @@ export default function MyApp({ Component, pageProps }) {
          gtag('config', 'G-45E6DTPQCV');
       `}
       </Script>
-      <Script>
-         {`(function(h,o,t,j,a,r){
+      <Script id="hotjar" strategy="lazyOnload">
+        {`(function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:3275002,hjsv:6};
             a=o.getElementsByTagName('head')[0];
