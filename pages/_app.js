3539739ded9af58bb30e24c232c168c8a45f4394
Matt-Cesco <matteo.francesconi88@gmail.com>
2022-12-07 19:03:28 +0000

    marketo munchkin

--- pages/_app.js
+++ pages/_app.js
@@ -26,6 +26,28 @@ export default function MyApp({ Component, pageProps }) {
          gtag('config', 'G-45E6DTPQCV');
       `}
       </Script>
+      <Script id="marketo-munchkin" strategy="lazyOnload">
+         {`(function() {
+            var didInit = false;
+            function initMunchkin() {
+               if(didInit === false) {
+                  didInit = true;
+                  Munchkin.init('CHANGE-ME');
+               }
+            }
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = '//munchkin.marketo.net/munchkin.js';
+            s.onreadystatechange = function() {
+               if (this.readyState == 'complete' || this.readyState == 'loaded') {
+                  initMunchkin();
+               }
+            };
+            s.onload = initMunchkin;
+            document.getElementsByTagName('head')[0].appendChild(s);
+            })();`}
+      </Script>
       <Script id="ruler-script">
         {`
          var __raconfig = __raconfig || {};
